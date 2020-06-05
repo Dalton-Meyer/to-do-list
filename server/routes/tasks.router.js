13b660174353b6b326862bbb38da65c58b80ba8f
Dalton Meyer <dalton.meyer94@gmail.com>
2020-06-05 17:54:30 -0500

    got postman to create a post and look at get

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -4,3 +4,29 @@ const pool = require('../modules/pool.js');
 const express = require('express');
 //access the router function
 const taskRouter = express.Router();
+
+taskRouter.get('/', (req, res) => {
+    let queryText = "SELECT * FROM to_do"
+    pool.query(queryText).then((result) => {
+        res.send(result.rows);
+    }).catch((error) => {
+        console.log(`Error making query: ${queryText}`);
+        res.sendStatus(500);
+    });
+});
+
+taskRouter.post('/', (req, res) => {
+    const todo = req.body;
+    const sDate = todo.date_start;
+    const eDate = todo.date_end;
+    const task = todo.task;
+    const done = todo.done;
+
+    if ((sDate !== undefined) || (eDate !== undefined) || (task !== undefined)) {
+        const queryText = `INSERT INTO to_do (date_start, date_end, task)
+        VALUES ($1, $2, $3)`
+        pool.query(queryText , [sDate, eDate, task]).then(() => { res.sendStatus(201)}).catch(() => {res.sendStatus(500)});
+    }else{ res.sendStatus(400)};
+});
+
+module.exports = taskRouter;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const taskRouter = require('./routes/tasks.router');
 const pool = require('./modules/pool');
-app.use(bodyParser.urlencoded({ extend: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('server/public'));
 
 app.use('/tasks', taskRouter);
@@ -12,3 +12,12 @@ app.use('/tasks', taskRouter);
 app.listen(PORT, () => {
     console.log('listening on port', PORT); 
 });
+// app.get("/dbtest", (req, res) => {
+//     // very simple DB test, just to make sure
+//     // that our database is connected correctly
+//     // GET http://localhost:5000/dbtest
+//     pool
+//          .query("Select 1;")
+//          .then(() => res.send("Database Connected Successfully!"))
+//          .catch((error) => res.send(`Error Connecting to Database: ${error}`));
+// });
